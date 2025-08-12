25512bec52c3b06c6acfc5f6aebef587714dff89
zoromask1020 <zoro67472@gmail.com>
2025-08-12 17:49:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,8 @@ import requests
 # Your n8n webhook URL (Test URL)
 N8N_WEBHOOK_URL = "https://202e1bfb0d1d.ngrok-free.app/webhook-test/cleaning"
 
+chat_id = st.query_params.get("chatid", [""])[0]
+
 st.title("Service Request Form")
 
 with st.form(key='service_form'):
@@ -19,13 +21,27 @@ if submit_button:
     if not (name and email and phone and rate and servicedesign):
         st.error("Please fill all the fields!")
     else:
+        else:
+        # Build Telegram confirmation message
+        text_message = (
+            f"*New Service Request Received!*\n\n"
+            f"Name: {name}\n"
+            f"Email: {email}\n"
+            f"Phone: {phone}\n"
+            f"Rate: {rate}\n"
+            f"Service Design: {servicedesign}\n"
+            f"✅ We’ll contact you shortly."
+        )
         # Data to send to n8n
         payload = {
             "name": name,
             "email": email,
             "phone": phone,
             "rate": rate,
-            "service_design": servicedesign
+            "service_design": servicedesign,
+            "chat_id": chat_id,
+            "text": text_message
+            
         }
 
         try:
