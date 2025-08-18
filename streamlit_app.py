17eb2c62bb4387ca24cadc086580d4f486dbf8f2
zoromask1020 <zoro67472@gmail.com>
2025-08-18 12:46:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import requests
 from datetime import datetime
 
 # Your n8n webhook URL (Test URL)
-N8N_WEBHOOK_URL = "https://a3cd41794bfa.ngrok-free.app/webhook/cleaning"
+N8N_WEBHOOK_URL = "https://a3cd41794bfa.ngrok-free.app/webhook-test/cleaning"
 
 # Get chat_id from query params
 chat_id = st.query_params.get("chatid", [""])
